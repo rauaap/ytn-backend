aa5c39d5fb323681e7595787bb41f8ae9796f639
apoleon <apoleon@apoleon-pc.com>
2022-01-20 16:50:04 +0200

    add HTTPS

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const https = require('https')
+const fs = require('fs')
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT)
-logger.info('http://127.0.0.1:3001')
+console.log(config.KEY_PATH, config.CERT_PATH)
+
+const server = https.createServer({
+    key: fs.readFileSync(config.KEY_PATH),
+    cert: fs.readFileSync(config.CERT_PATH),
+    passphrase: config.PASSPHRASE
+}, app)
+
+server.listen(config.PORT)
+logger.info(`http://127.0.0.1${config.PORT}`)
--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,15 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 const SECRET = process.env.SECRET
+const CERT_PATH = process.env.CERT_PATH
+const KEY_PATH = process.env.KEY_PATH
+const PASSPHRASE = process.env.PASSPHRASE
 
 module.exports = {
     PORT,
     MONGODB_URI,
-    SECRET
+    SECRET,
+    CERT_PATH,
+    KEY_PATH,
+    PASSPHRASE
 }
